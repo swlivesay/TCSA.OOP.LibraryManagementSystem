f4453cc2ca632d2fc3dc20a20bc6556da2a163a9
SWLivesay <swlivesay@gmail.com>
2025-01-27 22:01:37 -0800

    basic funtionality complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Spectre.Console;
+﻿using System.Net.Security;
+using Spectre.Console;
 
 
 string[] menuChoices = new string[3] {"View Books", "Add Book", "Delete Book"};
@@ -10,11 +11,67 @@ List<string> books = new List<string>()
     "Wuthering Heights", "Fahrenheit 451", "Catch-22", "The Hitchhiker's Guide to the Galaxy"
 };
 while (true)
-{    
+{   
+    Console.Clear(); 
+
     string choice = AnsiConsole.Prompt
     (
         new SelectionPrompt<string>()
         .Title("What do you want to do next?")
         .AddChoices(menuChoices)   
     );
+
+    switch (choice)
+    {
+        case "View Books":
+            AnsiConsole.MarkupLine("[yellow]List of Books:[/]");
+
+            foreach (string book in books)
+            {
+                AnsiConsole.MarkupLine($"- [cyan]{book}[/]");
+            }
+
+            AnsiConsole.MarkupLine("Press any key to  continue.");
+            Console.ReadKey();
+            break;
+        case "Add Book":
+            string title = AnsiConsole.Ask<string>("Enter the [green]title[/] of the bookto add:");
+
+            if (books.Contains(title))
+            {
+                AnsiConsole.MarkupLine("[red]This book already exists.[/]");
+            }
+            else
+            {
+                books.Add(title);
+                AnsiConsole.MarkupLine("[green]Book added successfully![/]");
+            }
+
+            AnsiConsole.MarkupLine("Press any key to continue.");
+            Console.ReadKey();
+            break;
+        case "Delete Book":
+            if (books.Count == 0)
+            {
+                AnsiConsole.MarkupLine("[red]No books available to delete");
+                Console.ReadKey();
+                return;
+            }
+
+            string bookToDelete = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                .Title("Select a [red]book[/] to delete:")
+                .AddChoices(books));
+            if (books.Remove(bookToDelete))
+            {
+                AnsiConsole.MarkupLine("[red]Book deleted succcessfully![/]");
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("[red]Book not found.[/]");
+            }
+
+            AnsiConsole.MarkupLine("Press any key to continue.");
+            break;
+    }
 }
