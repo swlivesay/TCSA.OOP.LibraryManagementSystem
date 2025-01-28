8d7ba069529218e6a937d4ee7362d4a2c9467718
SWLivesay <swlivesay@gmail.com>
2025-01-27 17:36:39 -0800

    First commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Spectre.Console;
+
+
+string[] menuChoices = new string[3] {"View Books", "Add Book", "Delete Book"};
+List<string> books = new List<string>()
+{
+    "The Great Gatsby", "To Kill a Mockingbird", "1984", "Pride and Prejudice", "The Catcher in the Rye", 
+    "The Hobbit", "Moby-Dick", "War and Peace", "The Odyssey", "The Lord of the Rings", "Jane Eyre", 
+    "Animal Farm", "Brave New World", "The Chronicles of Narnia", "The Diary of a Young Girl", "The Alchemist", 
+    "Wuthering Heights", "Fahrenheit 451", "Catch-22", "The Hitchhiker's Guide to the Galaxy"
+};
+while (true)
+{    
+    string choice = AnsiConsole.Prompt
+    (
+        new SelectionPrompt<string>()
+        .Title("What do you want to do next?")
+        .AddChoices(menuChoices)   
+    );
+}
